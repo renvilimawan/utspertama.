2dd6c7b17b2aadc3919f5fd8f326a5c06d835302
renvilimawan <renvil.imawan@gmail.com>
2023-05-11 16:37:55 +0700

    uts pbo

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-class Main {
-    public static void main(String[] args) {
+import mahasiswa.Mhs;
 
+class Main {
+
+    public static void main(String[] args) {
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+        
+        mhs.setNama("Renvil Imawan ");
+        mhs.setNim("G.111.20.0032");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("Password: " + mhs.getPassword());
     }
 }
