b6bc45914bf9fc878badb2139bc563271d9c4d81
renvilimawan <renvil.imawan@gmail.com>
2023-05-17 16:53:02 +0700

    Perbaikan UTS

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
-import mahasiswa.Mhs;
+import pengguna.User;
+import pengguna.Mhs;
 
 class Main {
-
     public static void main(String[] args) {
-        Mhs mhs = new Mhs("ini_username", "ini_password");
         
-        mhs.setNama("Renvil Imawan ");
+        Mhs mhs = new Mhs("username", "password");
+
+        mhs.setNama("Renvil Imawan");
         mhs.setNim("G.111.20.0032");
         mhs.setSmt(6);
 
-        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nama Mahasiswa: " + mhs.getNama());
         System.out.println("Nim Mhs: " + mhs.getNim());
         System.out.println("Smt Mhs: " + mhs.getSmt());
-        System.out.println("Nama Mhs: " + mhs.getNama());
-        System.out.println("Username: " + mhs.getUsername());
-        System.out.println("Password: " + mhs.getPassword());
+
+        User mahsiswa = new User("renvil.imawan", "qwerty");
+    
+    System.out.println("Username :" + mahsiswa.getUsername());
+    System.out.println("Password :" + mahsiswa.getPassword());
     }
 }
